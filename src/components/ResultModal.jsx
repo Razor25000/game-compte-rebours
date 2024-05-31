eeb31a087cfb35d37a7cd4ff0a8aee70d1331018
Regis Laffond <regis.laffond@yahoo.fr>
2024-05-31 13:02:06 +0200

    Enhancing the Demo App result Modal & Closing the Modal via the ESC key

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -13,13 +13,16 @@ const ResultModal = forwardRef(({targetTime, remainingTime, onReset}, ref) => {
   ); 
   const userLost = remainingTime <= 0;
   const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+  const score = Math.round((1 - remainingTime / (targetTime*1000))*100);
   
   return (
-    <dialog className="result-modal" ref={dialogRef}>
-      <h2>{userLost ? "You lost!" : "You won!"}</h2>
-      
+    <dialog className="result-modal" ref={dialogRef} onClose={onReset}>
+      {userLost && <h2>You lost!</h2>}
+      {!userLost && <h2>Your score is {score}</h2>}
+      {userLost && <p>The target time was {targetTime} seconds</p>}
       <p>The target time was {targetTime} seconds</p>
       <p>You stopped<strong> {formattedRemainingTime} seconds early</strong></p>
+      
       <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
